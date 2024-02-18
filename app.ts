2efc3f820ab0227299409a6eba50407a49e60b51
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-02-18 19:22:26 +0500

    version 1

--- app.ts
+++ app.ts
@@ -1,3 +1,47 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
-import show
+import {
+  displayTitleAndTagline,
+  countWordsInParagraph,
+  countCharactersInParagraph,
+  counterSpinner,
+  continueOrQuit,
+} from "./functions.js";
+
+export const start = async () => {
+  // function to prompt the user to enter an English paragraph and select an option
+  const { paragraph, selectedOption } = await inquirer.prompt([
+    {
+      name: "paragraph",
+      type: "input",
+      message: chalk.green(`\n\n Enter a paragraph in English: `),
+    },
+    {
+      name: "selectedOption",
+      type: "list",
+      choices: ["Count the words", "Count the characters"],
+      message: chalk.green(`\n Select an option`),
+    },
+  ]);
+  if (selectedOption === "Count the words") {
+    const numOfWords = countWordsInParagraph(paragraph);
+    counterSpinner({
+      text: chalk.gray(
+        `The number of words in given paragraph is: ${numOfWords}`
+      ),
+      color: "green",
+    });
+  } else if (selectedOption === "Count the characters") {
+    const numberOfCharacters = countCharactersInParagraph(paragraph);
+    counterSpinner({
+      text: chalk.gray(
+        `The number of characters in given paragraph is: ${numberOfCharacters}`
+      ),
+      color: "green",
+    });
+  }
+  //   continueOrQuit();
+};
+
+await displayTitleAndTagline();
+start();
---
+++ functions.ts
@@ -0,0 +1,58 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { start } from "./app.js";
+import showBanner from "node-banner";
+import { createSpinner } from "nanospinner";
+
+// function to display "currency convertor" title and tagline
+export const displayTitleAndTagline = async () => {
+  console.clear();
+  await showBanner(
+    `      Word Counter`,
+    `  This CLI app can count the number of words and characters in a paragraph
+   ________________________________________________________________________`,
+    "green",
+    "gray"
+  );
+};
+
+// function to display message after quitting the
+export const thanks = async () => {
+  await showBanner(`Thanks for trying`, undefined, "green");
+};
+
+// function to find number of words in a paragraph
+export const countWordsInParagraph = (paragraph: string): number => {
+  const words = paragraph.split(" ");
+  return words.length;
+};
+
+// function to find number of characters in a paragraph
+export const countCharactersInParagraph = (paragraph: string): number => {
+  paragraph = paragraph.replace(/ /g, "");
+  const characters = paragraph.split("");
+  return characters.length;
+};
+
+// spinner function
+export const counterSpinner = async (message: { [key: string]: any }) => {
+  const spinner = createSpinner(chalk.red(`Counting...`)).start();
+
+  setTimeout(() => {
+    spinner.success(message);
+  }, 3000);
+};
+
+// function to ask user to use or quit the app
+export const continueOrQuit = async () => {
+  const options = await inquirer.prompt({
+    name: "selected",
+    type: "list",
+    choices: ["Use the app again", "Quit app"],
+  });
+  if (options.selected == "Continue") {
+    start();
+  } else {
+    thanks();
+  }
+};
